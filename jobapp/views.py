3dba3dcb5495d18ffa5593860f3c8a68c6f350d9
keerthu000 <keerthanavadath@gmail.com>
2023-06-14 00:30:32 +0530

    first commit

--- jobapp/views.py
+++ jobapp/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def applyjob(request):
+    return render(request,'applyjob.html')
+
+    
+    
